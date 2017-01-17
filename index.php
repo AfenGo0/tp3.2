2eb1a96bd3732417300621897ca584a065461470
wodeccu <wodecc_u@sina.com>
2017-01-17 13:15:23 +0800

    +入口文件中，修改了默认的APP_PATH,讲Application修改成了Apps

--- index.php
+++ index.php
@@ -14,11 +14,11 @@
 // 检测PHP环境
 if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 
-// 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
+// 开启调试模式 建议开发阶段开启 部署阶段 注释或者设为false
 define('APP_DEBUG',True);
 
-// 定义应用目录
-define('APP_PATH','./Application/');
+// 定义应用目录,把默认的application修改成Apps
+define('APP_PATH','./Apps/');
 
 // 引入ThinkPHP入口文件
 require './ThinkPHP/ThinkPHP.php';
